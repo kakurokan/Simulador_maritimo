11bfedc1cc4f41b1509283de308fb72c24866005
kakurokan <leokakali7@gmail.com>
2026-04-23 17:25:39 +0100

    Refactor: Implement `Obstaculo` interface in `Tempestade`, add `intersect` method

--- src/Tempestade.java
+++ src/Tempestade.java
@@ -1,8 +1,13 @@
 import java.util.List;
 
-public class Tempestade {
+public class Tempestade implements Obstaculo {
     private Circulo area;
 
     public Tempestade(List<Route> routes) {
+    }
+
+    @Override
+    public List<Ponto> intersect(Route rota) {
+        return area.intersect(rota);
     }
 }
